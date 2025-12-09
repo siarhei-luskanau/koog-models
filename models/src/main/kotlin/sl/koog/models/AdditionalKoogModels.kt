a5693080ae05cacb94ef30b0273df37c24011174
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2025-12-09 09:52:23 +0100

    Add gpt-oss:20b Ollama model

--- models/src/main/kotlin/sl/koog/models/AdditionalKoogModels.kt
+++ models/src/main/kotlin/sl/koog/models/AdditionalKoogModels.kt
@@ -1,3 +1,26 @@
 package sl.koog.models
 
-object AdditionalKoogModels
+import ai.koog.prompt.llm.LLMCapability
+import ai.koog.prompt.llm.LLMProvider
+import ai.koog.prompt.llm.LLModel
+
+object AdditionalKoogModels {
+    object Ollama {
+        val GPT_OSS_20B: LLModel =
+            // "https://ollama.com/library/gpt-oss"
+            LLModel(
+                provider = LLMProvider.Ollama,
+                id = "gpt-oss:20b",
+                capabilities =
+                    listOf(
+                        LLMCapability.Completion,
+                        LLMCapability.Schema.JSON.Standard,
+                        LLMCapability.Speculation,
+                        LLMCapability.Temperature,
+                        LLMCapability.ToolChoice,
+                        LLMCapability.Tools,
+                    ),
+                contextLength = 128_000,
+            )
+    }
+}
