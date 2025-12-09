1233663312a2c33490c58d09717c4cbf5a9d210b
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2025-12-09 09:19:55 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,34 @@
+rootProject.name = "AdditionalKoogModels"
+
+include(":models")
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+                includeGroupByRegex("android.*")
+            }
+        }
+        gradlePluginPortal()
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+                includeGroupByRegex("android.*")
+            }
+        }
+        mavenCentral()
+        mavenLocal()
+    }
+}
