25d2ac1bd3a4f8070dec5f81cda73ad18d49ba4e
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2025-12-14 20:29:21 +0100

    Add qwen3-vl Ollama model

--- models/src/main/kotlin/sl/koog/models/AdditionalKoogModels.kt
+++ models/src/main/kotlin/sl/koog/models/AdditionalKoogModels.kt
@@ -84,5 +84,24 @@ object AdditionalKoogModels {
                     ),
                 contextLength = 128_000,
             )
+        val QWEN3_VL_2B: LLModel =
+            // "https://ollama.com/library/qwen3-vl"
+            LLModel(
+                provider = LLMProvider.Ollama,
+                id = "qwen3-vl:2b",
+                capabilities =
+                    listOf(
+                        LLMCapability.Document,
+                        LLMCapability.Schema.JSON.Basic,
+                        LLMCapability.Temperature,
+                        LLMCapability.Tools,
+                        LLMCapability.Vision.Image,
+                    ),
+                contextLength = 256_000,
+            )
+        val QWEN3_VL_4B: LLModel = QWEN3_VL_2B.copy(id = "qwen3-vl:4b")
+        val QWEN3_VL_8B: LLModel = QWEN3_VL_2B.copy(id = "qwen3-vl:8b")
+        val QWEN3_VL_30B: LLModel = QWEN3_VL_2B.copy(id = "qwen3-vl:30b")
+        val QWEN3_VL_32B: LLModel = QWEN3_VL_2B.copy(id = "qwen3-vl:32b")
     }
 }
